219ec5e43c8398fc5cc84d8b9828e263faaa1661
Piyush Deshmukh <p.desh31@gmail.com>
2015-01-24 12:22:26 +0530

    Update asciiart.py

--- asciiart.py
+++ asciiart.py
@@ -67,7 +67,6 @@ if __name__ == "__main__" :
 	FILE = open("asciiart.cpp", 'w')
 	FILE.write(out)
 	FILE.close()
-	os.system("cd ~/Desktop/codes")
 	os.system("make asciiart")
 	print ("\n\n\n")
 	temp = str(os.system("./asciiart"))
